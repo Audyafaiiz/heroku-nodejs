e2bcc34011249e8e92b6a15b0820f2b3128b0dfc
KOMPTUK-14 <audyafaiz@gmail.com>
2022-06-04 09:49:06 +0700

    anjay kamu boong

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const app = express()
 app.get('/', function (req, res) {
   res.json({
     number: 1,
-    nama: 'Okyza Maherdy Prabowo',
-    npm: '33220025'
+    nama: 'Audya Ramanda Putra',
+    npm: '2042017'
   });
 })
 
